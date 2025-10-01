a029819a81c23c815b2515e5a9979ac9a93f3aba
Your Name <your.email@example.com>
2025-10-01 12:56:49 +0900

    初期画面　２

--- app.js
+++ app.js
@@ -3660,6 +3660,7 @@ class App {
             // スキルレベル関連のデータを削除
             localStorage.removeItem('playerSkillLevel');
             localStorage.removeItem('playerSkillLevelData');
+            localStorage.removeItem('skillLevel');
 
             // コーチング関連のデータを削除
             localStorage.removeItem('coaching_user_progress');
